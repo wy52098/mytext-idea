04d7999c1ad2d9351bce38e28ee545622ad1072d
wy <937904805@qq.com>
2020-01-12 12:46:01 +0800

    123

---
+++ src/main/java/com/itheima/test/User.java
@@ -0,0 +1,30 @@
+package com.itheima.test;
+
+/****
+ *  @Author:wengyang
+ *  @Date:2020/1/12 11:24
+ *
+ */
+public class User {
+
+    private Integer age;
+    private String name;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
